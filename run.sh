32d95e6516fd3e19ca813ee9b912fa668c62e54a
lspongebobjh <lspongebobjh@gmail.com>
2023-01-19 08:00:54 +0000

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python cifar.py --dataset cifar10 --arch resnet --depth 20 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet20_cifar10 >/mnt/jiahanli/checkpoint/resnet20_cifar10.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 python cifar.py --dataset cifar10 --arch resnet --depth 32 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet32_cifar10 >/mnt/jiahanli/checkpoint/resnet32_cifar10.log 2>&1 &
-CUDA_VISIBLE_DEVICES=2 python cifar.py --dataset cifar10 --arch resnet --depth 44 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet44_cifar10 >/mnt/jiahanli/checkpoint/resnet44_cifar10.log 2>&1 &
-CUDA_VISIBLE_DEVICES=3 python cifar.py --dataset cifar10 --arch resnet --depth 56 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet56_cifar10 >/mnt/jiahanli/checkpoint/resnet56_cifar10.log 2>&1 &
-CUDA_VISIBLE_DEVICES=4 python cifar.py --dataset cifar100 --arch resnet --depth 20 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet20_cifar100 >/mnt/jiahanli/checkpoint/resnet20_cifar100.log 2>&1 &
-CUDA_VISIBLE_DEVICES=5 python cifar.py --dataset cifar100 --arch resnet --depth 32 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet32_cifar100 >/mnt/jiahanli/checkpoint/resnet32_cifar100.log 2>&1 &
-CUDA_VISIBLE_DEVICES=6 python cifar.py --dataset cifar100 --arch resnet --depth 44 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet44_cifar100 >/mnt/jiahanli/checkpoint/resnet44_cifar100.log 2>&1 &
-CUDA_VISIBLE_DEVICES=7 python cifar.py --dataset cifar100 --arch resnet --depth 56 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet56_cifar100 >/mnt/jiahanli/checkpoint/resnet56_cifar100.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python cifar.py --dataset cifar10 --arch resnet --depth 20 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet20_cifar10 >/mnt/jiahanli/checkpoints/resnet20_cifar10.log 2>&1 &
+CUDA_VISIBLE_DEVICES=1 python cifar.py --dataset cifar10 --arch resnet --depth 32 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet32_cifar10 >/mnt/jiahanli/checkpoints/resnet32_cifar10.log 2>&1 &
+CUDA_VISIBLE_DEVICES=2 python cifar.py --dataset cifar10 --arch resnet --depth 44 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet44_cifar10 >/mnt/jiahanli/checkpoints/resnet44_cifar10.log 2>&1 &
+CUDA_VISIBLE_DEVICES=3 python cifar.py --dataset cifar10 --arch resnet --depth 56 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet56_cifar10 >/mnt/jiahanli/checkpoints/resnet56_cifar10.log 2>&1 &
+CUDA_VISIBLE_DEVICES=4 python cifar.py --dataset cifar100 --arch resnet --depth 20 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet20_cifar100 >/mnt/jiahanli/checkpoints/resnet20_cifar100.log 2>&1 &
+CUDA_VISIBLE_DEVICES=5 python cifar.py --dataset cifar100 --arch resnet --depth 32 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet32_cifar100 >/mnt/jiahanli/checkpoints/resnet32_cifar100.log 2>&1 &
+CUDA_VISIBLE_DEVICES=6 python cifar.py --dataset cifar100 --arch resnet --depth 44 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet44_cifar100 >/mnt/jiahanli/checkpoints/resnet44_cifar100.log 2>&1 &
+CUDA_VISIBLE_DEVICES=7 python cifar.py --dataset cifar100 --arch resnet --depth 56 --workers 8 --checkpoint /mnt/jiahanli/checkpoints/resnet56_cifar100 >/mnt/jiahanli/checkpoints/resnet56_cifar100.log 2>&1 &
